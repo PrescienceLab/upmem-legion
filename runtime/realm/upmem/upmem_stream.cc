f96c7283c2e145349187c972752008810d59c221
krasow <krasow@u.northwestern.edu>
2025-05-22 10:30:55 -0500

    remove DPU_CALLBACK_SINGLE_CALL

--- runtime/realm/upmem/upmem_stream.cc
+++ runtime/realm/upmem/upmem_stream.cc
@@ -106,8 +106,7 @@ namespace Realm {
       // do a callback here
       CHECK_UPMEM(dpu_callback(*(this->get_stream()), &upmem_start_callback, (void *)e,
                                (dpu_callback_flags_t)(DPU_CALLBACK_ASYNC |
-                                                      DPU_CALLBACK_NONBLOCKING |
-                                                      DPU_CALLBACK_SINGLE_CALL)));
+                                                      DPU_CALLBACK_NONBLOCKING)));
 
       log_stream.debug() << "UPMEM fence event " << e << " recorded on stream " << stream
                          << " (DPU " << dpu << ")";
@@ -122,8 +121,7 @@ namespace Realm {
       // do a callback here
       CHECK_UPMEM(dpu_callback(*(this->get_stream()), &upmem_start_callback, (void *)e,
                                (dpu_callback_flags_t)(DPU_CALLBACK_ASYNC |
-                                                      DPU_CALLBACK_NONBLOCKING |
-                                                      DPU_CALLBACK_SINGLE_CALL)));
+                                                      DPU_CALLBACK_NONBLOCKING)));
 
       log_stream.debug() << "UPMEM start event " << e << " recorded on stream " << stream
                          << " (DPU " << dpu << ")";
@@ -149,8 +147,7 @@ namespace Realm {
       // do a callback here
       CHECK_UPMEM(dpu_callback(*(this->get_stream()), &upmem_start_callback, (void *)e,
                                (dpu_callback_flags_t)(DPU_CALLBACK_ASYNC |
-                                                      DPU_CALLBACK_NONBLOCKING |
-                                                      DPU_CALLBACK_SINGLE_CALL)));
+                                                      DPU_CALLBACK_NONBLOCKING)));
 
       add_event(e, 0, notification, 0);
     }
